99aa97bb6913ce2d99fc182e79d14d8874c14f96
Voxel <tux@xyecoc.com>
2026-03-23 16:20:10 +1000

    Delete Regedit.h

--- Regedit.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef REGEDIT_H
-#define REGEDIT_H
-
-void CheckRun(char* RUN_PATH);
-void CheckAndResetWinlogonValues(char* DEFAULT_USERINIT, char* DEFAULT_SHELL, char* WINLOGON_PATH);
-
-#endif
