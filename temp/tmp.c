7934642ac7132cef27a1bf8fc188fb07ced93154
Voxel <tux@xyecoc.com>
2026-03-23 16:42:02 +1000

    refactoring

--- temp/tmp.c
+++ temp/tmp.c
@@ -1,20 +1,21 @@
 #include "../pages/pages.h"
 #include <stdio.h>
-#include "../pages/pages.h"
 #include <stdlib.h>
 #include <Windows.h>
+
+
 
 void tmp_page() {
 	Intro();
 
-	int result = system("del /q /s %temp%\\*"), result2 = system("del /q /s C:\\Windows\\Temp\\*");
+	printf("Clean temp folders? (y/n): ");
+	if (getchar() != 'y') return;
 
-	if (result == 0 && result2 == 0) {
-		printf("Successfully cleaned temp :D ");
-	}
-	else {
-		printf("not cleaned. temp (run as admin?)");
-	}
+	int temp = system("del /q /s %temp%\\*");
+	int windowstemp = system("del /q /s C:\\Windows\\Temp\\*");
+
+	if (temp != 0) printf("Failed to clean temp.. maybe run as admin?");
+	if (windowstemp != 0) printf("Failed to clean C:\\Windows\\Temp.. maybe run as admin?");
 
 	Sleep(2000);
 
