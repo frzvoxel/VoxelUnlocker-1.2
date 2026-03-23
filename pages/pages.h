229fad88dcc45a17c98e637a65718d0fc523c9e2
Voxel <tux@xyecoc.com>
2026-03-23 16:22:44 +1000

    Unlocker Pages

---
+++ pages/pages.h
@@ -0,0 +1,16 @@
+#ifndef PAGES_H
+#define PAGES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	void NEXTPAGE_STARTUP(void);
+	void hosts_page(void);
+	void tmp_page(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
